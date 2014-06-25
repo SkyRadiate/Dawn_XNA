7dcc7f54cd9d8dcd77b860a28de7fb87177443a8
SkyZH <iSkyZH@163.com>
2014-06-25 20:03:49 +0800

    计划了Font类
    [伪日更]正在考虑Resource和Font类怎么继承，似乎要在Load时加上参数……【太凶残了，又要改代码】

---
+++ Source/Dawn/Dawn/Engine/Resource/Font.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dawn.Engine.Resource
+{
+	public class Font : Resource
+	{
+		public static byte MaxFontSize = 72;
+		public static byte DefaultFontSize = 16;
+		private System.Drawing.Font _font;
+		public Font()
+            : base()
+        {
+        }
+        public Font(string filename)
+            : base(filename)
+        {
+        }
+		~Font()
+        {
+            Dispose();
+        }
+		public string name
+		{
+			get { return _font.Name; }
+		}
+		public int size
+		{
+			get { return (int)_font.Size; }
+		}
+		public bool isBlod
+		{
+			get { return _font.Bold; }
+		}
+		public bool isItalic
+		{
+			get { return _font.Italic; }
+		}
+		public bool isUnderline
+		{
+			get { return _font.Underline; }
+		}
+	}
+}
